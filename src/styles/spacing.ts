51700cae1060ac77493d486651b1aa10860c8155
mriabokon <mriabokon@liveperson.com>
2021-08-02 18:57:27 +0300

    implemented Spacing stuff

--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,3 +1,25 @@
+import { getRem } from './common';
+
+const spaces = {
+  xxs: '4px',
+  xs: '8px',
+  s: '12px',
+  m: '20px',
+  l: '32px',
+  xl: '52px',
+  xxl: '84px',
+}
+
+const spacesInner = {
+  xxs: '4px',
+  xs: '8px',
+  s: '12px',
+  m: '16px',
+  l: '24px',
+  xl: '20px',
+  xxl: '32px',
+}
+
 const spacing = {
   xxs: '0.25rem',
   xs: '0.5rem',
@@ -7,5 +29,9 @@ const spacing = {
   xl: '3.25rem',
   xxl: '5.25rem',
 }
+
+export const getSpace = (size: keyof typeof spaces, inner = false): string => (inner ? spacesInner : spaces)[size];
+
+export const getSpaceRem = (size: keyof typeof spaces, inner = false): string => getRem(getSpace(size, inner));
 
 export default spacing;

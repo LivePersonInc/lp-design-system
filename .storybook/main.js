c8ca28c8d191f9248e0553d23935ad68f07dd476
mriabokon <mriabokon@liveperson.com>
2021-08-26 18:46:22 +0300

    Made Storybook works with web-components directly

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,25 @@ module.exports = {
   webpackFinal: async config => {
     config.resolve.alias = { ...config.resolve.alias, ...webpackAlias };
 
+    const oneOfRuleIndex = config.module.rules.findIndex(item => item.oneOf);
+
+    if (oneOfRuleIndex !== -1) {
+      const cssRuleIndex = config.module.rules[oneOfRuleIndex].oneOf.findIndex(rule => '.css'.match(rule.test));
+      if (cssRuleIndex !== -1) {
+        config.module.rules[oneOfRuleIndex].oneOf[cssRuleIndex].use = ['to-string-loader', 'css-loader'];
+      }
+
+      const scssRuleIndex = config.module.rules[oneOfRuleIndex].oneOf.findIndex(rule => '.scss'.match(rule.test));
+      if (scssRuleIndex !== -1) {
+        config.module.rules[oneOfRuleIndex].oneOf[scssRuleIndex].use = ['to-string-loader', 'css-loader', 'sass-loader'];
+      }
+
+      config.module.rules[oneOfRuleIndex].oneOf.unshift({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+    }
+
     return config;
   },
-}
+};

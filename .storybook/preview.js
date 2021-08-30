b6e45ad75f26db2bb3ca0c59ce96862301058873
mriabokon <mriabokon@liveperson.com>
2021-08-30 16:45:22 +0300

    Added LPDS theme for the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import LPDSTheme from './LPDSTheme';
+
+export const parameters = {
+  docs: {
+    theme: LPDSTheme,
+  },
+};
+
 export const decorators = [
   (Story, { args }) => (
     <div className="story-decorator" style={{ backgroundColor: (args.theme === 'dark' ? '#07092E' : '#FFF') }}>

c8ea048ee3c4278c07a9a83d7c15dba98a7b26aa
mriabokon <mriabokon@liveperson.com>
2021-08-31 16:24:01 +0300

    Changed Storybook UI
    - moved panel to the right
    - disabled shortcuts
    - disabled fullscreen button

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,5 +3,10 @@ import { addons } from '@storybook/addons';
 import LPDSTheme from './LPDSTheme';
 
 addons.setConfig({
+  panelPosition: 'right',
+  enableShortcuts: false,
   theme: LPDSTheme,
+  toolbar: {
+    fullscreen: { hidden: false },
+  },
 });

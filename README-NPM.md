67c31a0d5918351988d6022035712944c9a7e7be
mriabokon <mriabokon@liveperson.com>
2021-09-07 16:00:24 +0300

    Improved npm publishing process

---
+++ README-NPM.md
@@ -0,0 +1,44 @@
+# LivePerson Design System
+
+Checkout the [demos](https://livepersoninc.github.io/lp-design-system)
+
+## Installing
+
+You can install LPDS with [NPM](https://www.npmjs.com/package/@liveperson/design-system) or Yarn.
+
+- NPM: `npm i @liveperson/design-system`
+- YARN: `yarn add @liveperson/design-system`
+
+## How to use
+
+You can use any LPDS component either in a browser or with a front-end framework
+
+### Browser usage
+```html
+<html>
+  <head>
+    <script src="button.js" />
+  </head>
+
+  <body>
+    <lp-button labal="button" />
+  </body>
+</html>
+```
+
+### Framework usage (React)
+```jsx
+import '@liveperson/design-system/button';
+
+const App = () => {
+  return (
+    <lp-button label="Button" />
+  );
+};
+
+export default App
+```
+
+## License
+
+MIT © [LivePersonInc](https://github.com/LivePersonInc)
---
+++ scripts/release.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const packageJSON = require('../package.json');
+
+const buildPath = path.resolve(__dirname, '../build');
+
+if (!fs.existsSync(buildPath)) {
+  execSync('yarn build', { stdio: 'inherit' });
+}
+
+console.log('\n');
+
+console.log('Preparing package.json');
+
+const fields = {
+  main: 'index.js',
+  files: null,
+  scripts: null,
+  dependencies: null,
+  devDependencies: null,
+  eslintConfig: null,
+  browserslist: null,
+  jest: null,
+  'config-overrides-path': null,
+};
+
+Object.keys(fields).forEach(key => {
+  if (!packageJSON[key]) {
+    return true;
+  }
+
+  if (fields[key] === null) {
+    delete packageJSON[key];
+
+    return true;
+  }
+
+  packageJSON[key] = (typeof fields[key] === 'object' ? { ...packageJSON[key], ...fields[key] } : fields[key]);
+});
+
+fs.writeFileSync(
+  path.resolve(__dirname, buildPath, 'package.json'),
+  JSON.stringify(packageJSON, null, 2),
+);
+
+console.log('Preparing README file');
+
+fs.copyFileSync(path.resolve(__dirname, '../README-NPM.md'), path.resolve(__dirname, buildPath, 'README.md'));
+
+console.log('\n');

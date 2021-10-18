8b08e118e79d1608a17ebc3aca83555d6c05054d
mriabokon <mriabokon@liveperson.com>
2021-10-18 16:16:28 +0300

    Removed path alias for LPDS components package

--- packages/components/direflow-webpack.js
+++ packages/components/direflow-webpack.js
@@ -1,11 +1,7 @@
 const { webpackConfig } = require('direflow-scripts');
 
-const webpackAlias = require('./webpack-alias');
-
 module.exports = (config, env) => {
   config = webpackConfig(config, env);
-
-  config.resolve.alias = { ...config.resolve.alias, ...webpackAlias };
 
   if (env === 'production') {
     delete config.entry['path-0'];

ca3856c01ece8f6cf6748251f01cf8a7fcf815e1
mriabokon <mriabokon@liveperson.com>
2021-10-22 17:42:38 +0300

    Added scaleValue helper function

--- packages/components/src/common/helpers.ts
+++ packages/components/src/common/helpers.ts
@@ -13,3 +13,15 @@ export function getPropsByDefaultProps<P extends Object = Object>(defaultProps: 
 
   return newProps;
 }
+
+export function scaleValue(value: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+  const result = (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
+  if (result < outMin) {
+    return outMin;
+  } else if (result > outMax) {
+    return outMax;
+  }
+
+  return result;
+}

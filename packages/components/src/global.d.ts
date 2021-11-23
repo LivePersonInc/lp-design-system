b13176c415a1b3f589b65ee5a2ee9105f6ffff9d
mriabokon <mriabokon@liveperson.com>
2021-11-23 18:09:19 +0200

    fix(components): Fixed web-components declarations

--- packages/components/src/global.d.ts
+++ packages/components/src/global.d.ts
@@ -1,23 +1,23 @@
 /// <reference types="@liveperson-design-system/icons/src/global" />
 
-import { ButtonGroupProps } from 'lpds/components/button-group/ButtonGroup';
-import { ButtonProps } from 'lpds/components/button/Button';
-import { CheckboxProps } from 'lpds/components/checkbox/Checkbox';
-import { ChipProps } from 'lpds/components/chip/Chip';
-import { ChipGroupProps } from 'lpds/components/chip/ChipGroup';
-import { DropdownProps } from 'lpds/components/dropdown/Dropdown';
-import { FileInputProps } from 'lpds/components/file-input/FileInput';
-import { InputHelperProps } from 'lpds/components/input-helper/InputHelper';
-import { InputLabelProps } from 'lpds/components/input-label/InputLabel';
-import { NumberInputProps } from 'lpds/components/number-input/NumberInput';
-import { RadioProps } from 'lpds/components/radio/Radio';
-import { RadioGroupProps } from 'lpds/components/radio/RadioGroup';
-import { SelectOptionProps } from 'lpds/components/select/SelectOption';
-import { SelectProps } from 'lpds/components/select/Select';
-import { TextareaProps } from 'lpds/components/textarea/Textarea';
-import { TextInputProps } from 'lpds/components/text-input/TextInput';
-import { ToggleButtonProps } from 'lpds/components/toggle-button/ToggleButton';
-import { ToggleProps } from 'lpds/components/toggle/Toggle';
+import { ButtonGroupProps } from './components/button-group/ButtonGroup';
+import { ButtonProps } from './components/button/Button';
+import { CheckboxProps } from './components/checkbox/Checkbox';
+import { ChipProps } from './components/chip/Chip';
+import { ChipGroupProps } from './components/chip/ChipGroup';
+import { DropdownProps } from './components/dropdown/Dropdown';
+import { FileInputProps } from './components/file-input/FileInput';
+import { InputHelperProps } from './components/input-helper/InputHelper';
+import { InputLabelProps } from './components/input-label/InputLabel';
+import { NumberInputProps } from './components/number-input/NumberInput';
+import { RadioProps } from './components/radio/Radio';
+import { RadioGroupProps } from './components/radio/RadioGroup';
+import { SelectOptionProps } from './components/select/SelectOption';
+import { SelectProps } from './components/select/Select';
+import { TextareaProps } from './components/textarea/Textarea';
+import { TextInputProps } from './components/text-input/TextInput';
+import { ToggleButtonProps } from './components/toggle-button/ToggleButton';
+import { ToggleProps } from './components/toggle/Toggle';
 
 declare global {
   namespace JSX {

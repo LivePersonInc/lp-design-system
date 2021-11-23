e84ed4bf749ed608a6b7625a21b427b83cf92ce5
mriabokon <mriabokon@liveperson.com>
2021-11-23 19:00:18 +0200

    fix(tokens): Fixed types declarations for *.exports.scss files

--- packages/tokens/src/declaration.d.ts
+++ packages/tokens/src/declaration.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.scss' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '*.exports.scss' {
+  const content: Record<string, string>;
+  export default content;
+}

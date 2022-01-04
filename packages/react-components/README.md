7006f01a432276657b7419bc165e783827937b38
mriabokon <mriabokon@liveperson.com>
2022-01-04 15:19:00 +0200

    feat(react-components): Added react-components package

---
+++ packages/react-components/README.md
@@ -0,0 +1,30 @@
+# LivePerson Design System Components
+
+Checkout the [demos](https://livepersoninc.github.io/lp-design-system/?path=/story/components)
+
+## Installing
+
+You can install LPDS components with NPM or Yarn.
+
+- YARN: `yarn add @liveperson-design-system/react-components`
+- NPM: `npm i @liveperson-design-system/react-components`
+
+## How to use
+
+```jsx
+import React from 'react';
+
+import { LpButton } from '@liveperson-design-system/react-components';
+
+const App = () => {
+  return (
+    <LpButton>Button</LpButton>
+  );
+};
+
+export default App
+```
+
+## License
+
+MIT © [LivePersonInc](https://github.com/LivePersonInc)
---
+++ packages/react-components/src/components/stencil-generated/index.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@liveperson-design-system/components';
+
+import { defineCustomElements } from '@liveperson-design-system/components/loader';
+
+defineCustomElements();
+export const LpButton = /*@__PURE__*/createReactComponent<JSX.LpButton, HTMLLpButtonElement>('lp-button');
+export const LpButtonGroup = /*@__PURE__*/createReactComponent<JSX.LpButtonGroup, HTMLLpButtonGroupElement>('lp-button-group');
+export const LpCheckbox = /*@__PURE__*/createReactComponent<JSX.LpCheckbox, HTMLLpCheckboxElement>('lp-checkbox');
+export const LpChip = /*@__PURE__*/createReactComponent<JSX.LpChip, HTMLLpChipElement>('lp-chip');
+export const LpDropdown = /*@__PURE__*/createReactComponent<JSX.LpDropdown, HTMLLpDropdownElement>('lp-dropdown');
+export const LpFileInput = /*@__PURE__*/createReactComponent<JSX.LpFileInput, HTMLLpFileInputElement>('lp-file-input');
+export const LpInputHelper = /*@__PURE__*/createReactComponent<JSX.LpInputHelper, HTMLLpInputHelperElement>('lp-input-helper');
+export const LpInputLabel = /*@__PURE__*/createReactComponent<JSX.LpInputLabel, HTMLLpInputLabelElement>('lp-input-label');
+export const LpMultiselectChip = /*@__PURE__*/createReactComponent<JSX.LpMultiselectChip, HTMLLpMultiselectChipElement>('lp-multiselect-chip');
+export const LpNumberInput = /*@__PURE__*/createReactComponent<JSX.LpNumberInput, HTMLLpNumberInputElement>('lp-number-input');
+export const LpRadio = /*@__PURE__*/createReactComponent<JSX.LpRadio, HTMLLpRadioElement>('lp-radio');
+export const LpRadioGroup = /*@__PURE__*/createReactComponent<JSX.LpRadioGroup, HTMLLpRadioGroupElement>('lp-radio-group');
+export const LpSelect = /*@__PURE__*/createReactComponent<JSX.LpSelect, HTMLLpSelectElement>('lp-select');
+export const LpSelectOption = /*@__PURE__*/createReactComponent<JSX.LpSelectOption, HTMLLpSelectOptionElement>('lp-select-option');
+export const LpSliderInput = /*@__PURE__*/createReactComponent<JSX.LpSliderInput, HTMLLpSliderInputElement>('lp-slider-input');
+export const LpTextInput = /*@__PURE__*/createReactComponent<JSX.LpTextInput, HTMLLpTextInputElement>('lp-text-input');
+export const LpTextarea = /*@__PURE__*/createReactComponent<JSX.LpTextarea, HTMLLpTextareaElement>('lp-textarea');
+export const LpToggle = /*@__PURE__*/createReactComponent<JSX.LpToggle, HTMLLpToggleElement>('lp-toggle');
+export const LpToggleButton = /*@__PURE__*/createReactComponent<JSX.LpToggleButton, HTMLLpToggleButtonElement>('lp-toggle-button');

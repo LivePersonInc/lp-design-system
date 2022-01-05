9c31e7adf67c85606aa9f8745fc6e8dddf642e23
mriabokon <mriabokon@liveperson.com>
2022-01-05 18:14:56 +0200

    fix(docs): Updated docs

--- docs/common/packages.js
+++ docs/common/packages.js
@@ -1,7 +1,7 @@
 export const packagesScope = '@liveperson-design-system';
 
-export const packages = ['Tokens', 'Icons', 'Components'].map(title => ({
+export const packages = ['Tokens', 'Icons', 'Components', 'React-Components'].map(title => ({
   key: title.toLowerCase(),
   name: `${packagesScope}/${title.toLowerCase()}`,
-  title
+  title: title.replaceAll('-', ' '),
 }));
